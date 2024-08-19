c2989ca9d1c07bde36386c3b60d519aba2dfcad5
Adam Davidson <agdavidson91@gmail.com>
2024-08-19 07:18:54 -0400

    basics

--- basics.py
+++ basics.py
@@ -5,6 +5,10 @@ lname = "Davidson"
 canDrink = True
 lists = ["Sarah", 31, "Davidson", 33]
 sets = {"Stranger Things", "LOTR", "Matrix"}
+dicts = {
+    "golfer": "Tiger Woods",
+    "majors": 15
+}
 #### VARIABLES ####
 
 fullname = f'{fname} {lname}'
@@ -19,3 +23,4 @@ else:
     print("You can't drink yet!")
     
 
+print(dicts)
