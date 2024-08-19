6b52bb70d507c44511c46e933b59412482f11933
Adam Davidson <agdavidson91@gmail.com>
2024-08-19 07:15:05 -0400

    learning

---
+++ basics.py
@@ -0,0 +1,21 @@
+#### DATA TYPES ####
+age = 33
+fname = "Adam"
+lname = "Davidson"
+canDrink = True
+lists = ["Sarah", 31, "Davidson", 33]
+sets = {"Stranger Things", "LOTR", "Matrix"}
+#### VARIABLES ####
+
+fullname = f'{fname} {lname}'
+print(fullname)
+
+
+print(f'{fullname}s age is {age}')
+
+if age > 21:
+    print("You can drink!")
+else:
+    print("You can't drink yet!")
+    
+
