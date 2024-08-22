a9de917f030b1ac369cf7b3b8bd3b494f5be9ffa
Adam Davidson <agdavidson91@gmail.com>
2024-08-22 07:45:29 -0400

    asd

--- intermediate.py
+++ intermediate.py
@@ -98,3 +98,13 @@ for location in sales_data:
     scoops_sold += i
 print(scoops_sold)
 ######################################################################################
+user_name = "Dave"  
+match user_name:  
+    case "Dave":  
+        print("Get off my computer Dave!")  
+    case "angela_catlady_87":  
+        print("I know it is you, Dave! Go away!")   
+    case "Codecademy":  
+        print("Access Granted.")  
+    case default:  
+        print("Username not recognized.")  
