abf0ecbb98b315d13440304902373dce0157d9f5
Adam Davidson <agdavidson91@gmail.com>
2024-09-01 20:41:33 -0400

    l

--- useful.py
+++ useful.py
@@ -7,3 +7,19 @@ numbers = [1, 2, 3, 4, 5]
 doubled_numbers = map(double, numbers) 
 
 print(list(doubled_numbers))  # Output: [2, 4, 6, 8, 10] 
+############################################
+# Converting strings to integers 
+
+str_nums = ['1', '2', '3', '4', '5'] 
+
+int_nums = list(map(int, str_nums)) 
+
+print(int_nums)  # Output: [1, 2, 3, 4, 5]
+############################################
+# Finding the length of strings 
+
+words = ['apple', 'banana', 'cherry'] 
+
+word_lengths = list(map(len, words)) 
+
+print(word_lengths)  # Output: [5, 6, 6] 
